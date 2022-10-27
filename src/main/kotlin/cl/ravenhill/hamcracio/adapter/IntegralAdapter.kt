a71c4105828cccb7632b8bd0d9c89cb8b3c7531f
R8V <ignacio.slater@ug.uchile.cl>
2022-10-27 15:38:21 -0300

    DOC added docstring comments to IntegralAdapter

--- src/main/kotlin/cl/ravenhill/hamcracio/adapter/IntegralAdapter.kt
+++ src/main/kotlin/cl/ravenhill/hamcracio/adapter/IntegralAdapter.kt
@@ -1,12 +1,25 @@
 package cl.ravenhill.hamcracio.adapter
 
 
+/**
+ * Adapter for the hamster service.
+ */
 class IntegralAdapter {
-  private val hamster = IntegralHamster()
-  fun integrate(
-    name: String,
-    f: (Double) -> Double,
-    from: Double,
-    to: Double
-  ) = hamster.hamham(name, f, from, to)
+    /**
+     * The hamster service.
+     */
+    private val hamster = IntegralHamster()
+
+    /**
+     * Integrates the function f from a to b using the trapezoidal rule.
+     *
+     * @param name  The name of the function; for example `f`
+     * @param f     The implementation of the function to integrate
+     * @param from  The lower bound of the integral
+     * @param to    The upper bound of the integral
+     *
+     * @return  The value of the integral
+     */
+    fun integrate(name: String, f: (Double) -> Double, from: Double, to: Double) =
+        hamster.hamham(name, f, from, to)
 }
--- src/main/kotlin/cl/ravenhill/hamcracio/observer/Card.kt
+++ src/main/kotlin/cl/ravenhill/hamcracio/observer/Card.kt
@@ -1,4 +1,9 @@
 package cl.ravenhill.hamcracio.observer
 
-
+/**
+ * A card in the game.
+ *
+ * This interface is just used to illustrate the design patterns and has no concrete
+ * implementations.
+ */
 interface Card
