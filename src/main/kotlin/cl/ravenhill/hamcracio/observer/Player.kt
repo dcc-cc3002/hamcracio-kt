65b7a61f2d123fd4605b646f61bf82fde6c6b161
R8V <ignacio.slater@ug.uchile.cl>
2022-10-27 15:33:37 -0300

    DOC added docstring and comments to Player

--- src/main/kotlin/cl/ravenhill/hamcracio/observer/Player.kt
+++ src/main/kotlin/cl/ravenhill/hamcracio/observer/Player.kt
@@ -2,15 +2,29 @@ package cl.ravenhill.hamcracio.observer
 
 import kotlin.properties.Delegates
 
-// Player.kt
+/**
+ * A player in the game.
+ *
+ * @property playedCard The card played by the player.
+ *                      This is an observable property.
+ */
 class Player {
-  private val cardListeners = mutableListOf<CardObserver>()
+    private val cardListeners = mutableListOf<CardObserver>()
 
-  var playedCard by Delegates.observable(null) { _, _, new ->
-    cardListeners.parallelStream().forEach { it.onCardPlayed(new) }
-  }
+    // The observable delegate receives an initial value and a lambda.
+    // The lambda is called every time the property is changed; it receives 3 parameters:
+    // 1. The property itself
+    // 2. The old value
+    // 3. The new value
+    // Since the first two parameters are unused, they are replaced with the underscore character.
+    var playedCard: Card? by Delegates.observable(null) { _, _, new ->
+        // We notify all the listeners that the card has changed; parallelStream() is used to
+        // notify the listeners in parallel.
+        cardListeners.parallelStream().forEach { it.onCardPlayed(new) }
+    }
 
-  fun addCardListener(gameController: CardObserver) {
-    cardListeners.add(gameController)
-  }
+    /**
+     * Adds a listener to the player.
+     */
+    fun addCardListener(gameController: CardObserver) = cardListeners.add(gameController)
 }
